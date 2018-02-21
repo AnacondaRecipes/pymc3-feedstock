39c27bd9518d7e4d5cf62cfa0eea1ba73bc4a3ae
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-02-21 00:15:25 -0600

    Add more imports; fix run deps

---
+++ recipe/run_test.py
@@ -0,0 +1,16 @@
+import os
+# theano >= 1.0.0
+os.environ['MKL_THREADING_LAYER']='GNU'
+
+import pymc3
+import pymc3.backends
+import pymc3.distributions
+import pymc3.examples
+import pymc3.glm
+import pymc3.gp
+import pymc3.plots
+import pymc3.step_methods
+import pymc3.step_methods.hmc
+import pymc3.tests
+import pymc3.tuning
+import pymc3.variational
